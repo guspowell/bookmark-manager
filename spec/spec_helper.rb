42c21b19d7ba0957d28e124a7a35fca94486cce1
guspowell <augustuspowell@me.com>
2015-01-13 11:12:50 +0000

    pushing for josh

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -31,6 +31,7 @@ RSpec.configure do |config|
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
+  end
 
   config.before(:each) do
     DatabaseCleaner.start
