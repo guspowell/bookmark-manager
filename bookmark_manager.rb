afb98f5b7682cdfc45c34d137fa14277eea9f3df
guspowell <augustuspowell@me.com>
2015-01-13 14:48:08 +0000

    added tags options and test passes

--- bookmark_manager.rb
+++ bookmark_manager.rb
@@ -1,13 +1,14 @@
 require 'sinatra/base'
 require 'data_mapper'
 
+require 'link'
+require 'tag'
+
 class BookmarkManager < Sinatra::Base
 
   env = ENV['RACK_ENV'] || 'development'
 
   DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
-
-  require './lib/link'
 
   DataMapper.finalize
 
@@ -21,7 +22,10 @@ class BookmarkManager < Sinatra::Base
   post '/links' do
     url = params["url"]
     title = params["title"]
-    Link.create(:url => url, :title => title)
+    tags = params["tags"].split(" ").map do |tag|
+      Tag.first_or_create(:text => tag)
+    end
+    Link.create(:url => url, :title => title, :tags => tags)
     redirect to('/')
   end
 
